525b56346b57a6f30b5108df2beef1b2f19e2c44
laheri72 <idrislaheri72@gmail.com>
2026-05-02 07:49:24 +0530

    team to Room mapping

---
+++ config/terms.py
@@ -0,0 +1,12 @@
+# config/terms.py
+# Terminology mapping: internal code uses "Team", users see "Room".
+# To change what users see, edit ONLY this file.
+
+TERMS = {
+    'TEAM':         'Room',
+    'TEAMS':        'Rooms',
+    'TEAM_MEMBER':  'Member',
+    'TEAM_MEMBERS': 'Members',
+    'TEAM_BUILDER': 'Room Manager',
+    'MY_TEAMS':     'My Rooms',
+}
